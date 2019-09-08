d0a324fc7f8660248c2f9fdd5b30aa64bacd2db1
Arimsingle <Arim.coe60@gmail.com>
2019-09-09 03:07:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import card_game from './card_game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="first">
+        <div className="second">
+          <nav role="navigation">npm
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact Us</a>
+          </nav>
+          <div className="thrid">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
